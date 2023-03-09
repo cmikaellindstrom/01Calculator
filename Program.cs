3dc055e0ccb4cde464520b5842dc7d36a3294e08
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-03-09 19:19:40 +0100

    Add start comments and welcome the user to this program

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace Calculator
 {
+    /// <summary>
+    /// The class for this program
+    /// </summary>
     internal class Program
     {
+        // Name of this program
+        const string ProgramName = "The Calculator 2000";
+        
+        /// <summary>
+        /// The start point of this program
+        /// </summary>
+        /// <param name="args">Anything</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome the user to this program
+            Console.WriteLine($"Welcome to {ProgramName}!");
+
+            // Make a pause in the program
+            Console.ReadKey();
         }
     }
 }
